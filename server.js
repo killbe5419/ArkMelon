2b78bea864e563a43647b3625fd5fa21c0b09e32
killbe5419 <killbe.jp@gmail.com>
2020-06-12 14:32:49 +0900

    add new static hostings.

--- server.js
+++ server.js
@@ -19,6 +19,38 @@ app.get("/404",(req,res) => {
     res.sendFile(staticPath + "/pages/404.html");
 })
 
+app.get("/operators",(req,res) => {
+    res.sendFile(staticPath + "/pages/gameData/operators.html");
+})
+
+app.get("/enemies",(req,res) => {
+    res.sendFile(staticPath + "/pages/gameData/enemies.html");
+})
+
+app.get("/clothes",(req,res) => {
+    res.sendFile(staticPath + "/pages/gameData/clothes.html");
+})
+
+app.get("/maps",(req,res) => {
+    res.sendFile(staticPath + "/pages/gameData/maps.html");
+})
+
+app.get("/themes",(req,res) => {
+    res.sendFile(staticPath + "/pages/gameData/themes.html");
+})
+
+app.get("/events",(req,res) => {
+    res.sendFile(staticPath + "/pages/gameData/events.html");
+})
+
+app.get("/pools",(req,res) => {
+    res.sendFile(staticPath + "/pages/gameData/pools.html");
+})
+
+app.get("/items",(req,res) => {
+    res.sendFile(staticPath + "/pages/gameData/items.html");
+})
+
 app.listen(3333,() => {
     console.log("listening on http://localhost:3333\n");
 })
